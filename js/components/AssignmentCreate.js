cfeccadbf4ba33fdc6ff07721e1ed8cbae26e19b
Mahmoud-5067 <152265819+Mahmoud-5067@users.noreply.github.com>
2023-11-28 17:57:55 +0100

    Add files via upload

---
+++ js/components/AssignmentCreate.js
@@ -0,0 +1,23 @@
+export default {
+    template: `
+        <form @submit.prevent="add">
+            <div class="text-black border border-gray-600 my-6">
+                <input v-model="newAssignment" placeholder="New assignmnet..." class="p-2 outline-none ">
+                <button type="submit" class="bg-white p-2 border-l"> Add </button>
+            </div>
+        </form>
+    `,
+
+    data() {
+        return {
+          newAssignment: '',
+        };
+      },
+    
+      methods: {
+        add(e) {
+          this.$emit('add', this.newAssignment);
+          this.newAssignment = '';
+        },
+      },
+}
